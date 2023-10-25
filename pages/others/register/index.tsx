d0de5ee46d59d99dd6838c03b949bf52fe94c541
jhoanduran93 <jhoanduran933@gmail.com>
2023-10-24 23:28:36 -0300

    nuev

--- pages/others/register/index.tsx
+++ pages/others/register/index.tsx
@@ -76,7 +76,6 @@ function SignUp() {
       
     }
   };
-  
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     await createUser();
@@ -152,155 +151,157 @@ function SignUp() {
             </Text>
             <HSeparator />
           </Flex>
-          <FormControl as="form" onSubmit={handleSubmit}>
-            <FormLabel
-              cursor="pointer"
-              htmlFor="name"
-              display="flex"
-              ms="4px"
-              fontSize="sm"
-              fontWeight="500"
-              color={textColor}
-              mb="8px"
-            >
-              Name<Text color={brandStars}>*</Text>
-            </FormLabel>
-            <Input
-              isRequired={true}
-              variant="auth"
-              id="name"
-              fontSize="sm"
-              type={show ? 'text' : 'text'}
-              placeholder="Enter your name"
-              mb="24px"
-              size="lg"
-              borderColor={borderColor}
-              h="54px"
-              _placeholder={{ placeholderColor }}
-              fontWeight="500"
-              value={name}
-              onChange={handleNameChange}
-            />
-            <FormLabel
-              cursor="pointer"
-              display="flex"
-              ms="4px"
-              htmlFor="email"
-              fontSize="sm"
-              fontWeight="500"
-              color={textColor}
-              mb="8px"
-            >
-              Email<Text color={brandStars}>*</Text>
-            </FormLabel>
-            <Input
-              isRequired={true}
-              id="email"
-              variant="auth"
-              fontSize="sm"
-              type="email"
-              borderColor={borderColor}
-              placeholder="Enter your email address"
-              mb="24px"
-              size="lg"
-              _placeholder={{ placeholderColor }}
-              h="54px"
-              fontWeight="500"
-              value={email}
-              onChange={handleEmailChange}
-            />
-            {/* PASSWORD */}
-            <FormLabel
-              cursor="pointer"
-              ms="4px"
-              fontSize="sm"
-              fontWeight="500"
-              htmlFor="pass"
-              color={textColor}
-              display="flex"
-            >
-              Password<Text color={brandStars}>*</Text>
-            </FormLabel>
-            <InputGroup size="md">
-              <Input
-                isRequired={true}
-                variant="auth"
-                id="pass"
+          <form onSubmit={handleSubmit}>
+            <FormControl>
+              <FormLabel
+                cursor="pointer"
+                htmlFor="name"
+                display="flex"
+                ms="4px"
                 fontSize="sm"
-                placeholder="Enter your password"
-                mb="24px"
-                size="lg"
-                h="54px"
-                borderColor={borderColor}
                 fontWeight="500"
-                _placeholder={{ placeholderColor }}
-                type={show ? 'text' : 'password'}
-                value={password}
-                onChange={handlePasswordChange}
-              />
-              <InputRightElement display="flex" alignItems="center" mt="4px">
-                <Icon
-                  color={textColorSecondary}
-                  _hover={{ cursor: 'pointer' }}
-                  as={show ? RiEyeCloseLine : MdOutlineRemoveRedEye}
-                  onClick={handleClick}
-                />
-              </InputRightElement>
-            </InputGroup>
-            {/* CONFIRM */}
-            <FormLabel
-              cursor="pointer"
-              htmlFor="confirm"
-              ms="4px"
-              fontSize="sm"
-              fontWeight="500"
-              borderColor={borderColor}
-              color={textColor}
-              display="flex"
-            >
-              Confirm Password<Text color={brandStars}>*</Text>
-            </FormLabel>
-            <InputGroup size="md">
+                color={textColor}
+                mb="8px"
+              >
+                Name<Text color={brandStars}>*</Text>
+              </FormLabel>
               <Input
                 isRequired={true}
                 variant="auth"
+                id="name"
                 fontSize="sm"
-                placeholder="Enter your password again"
-                id="confirm"
+                type={show ? 'text' : 'text'}
+                placeholder="Enter your name"
                 mb="24px"
                 size="lg"
                 borderColor={borderColor}
                 h="54px"
-                fontWeight="500"
                 _placeholder={{ placeholderColor }}
-                type={show ? 'text' : 'password'}
-                value={confirmPassword}
-                onChange={handleConfirmPasswordChange}
+                fontWeight="500"
+                value={name}
+                onChange={handleNameChange}
               />
-              <InputRightElement display="flex" alignItems="center" mt="4px">
-                <Icon
-                  color={textColorSecondary}
-                  _hover={{ cursor: 'pointer' }}
-                  as={show ? RiEyeCloseLine : MdOutlineRemoveRedEye}
-                  onClick={handleClick}
+              <FormLabel
+                cursor="pointer"
+                display="flex"
+                ms="4px"
+                htmlFor="email"
+                fontSize="sm"
+                fontWeight="500"
+                color={textColor}
+                mb="8px"
+              >
+                Email<Text color={brandStars}>*</Text>
+              </FormLabel>
+              <Input
+                isRequired={true}
+                id="email"
+                variant="auth"
+                fontSize="sm"
+                type="email"
+                borderColor={borderColor}
+                placeholder="Enter your email address"
+                mb="24px"
+                size="lg"
+                _placeholder={{ placeholderColor }}
+                h="54px"
+                fontWeight="500"
+                value={email}
+                onChange={handleEmailChange}
+              />
+              {/* PASSWORD */}
+              <FormLabel
+                cursor="pointer"
+                ms="4px"
+                fontSize="sm"
+                fontWeight="500"
+                htmlFor="pass"
+                color={textColor}
+                display="flex"
+              >
+                Password<Text color={brandStars}>*</Text>
+              </FormLabel>
+              <InputGroup size="md">
+                <Input
+                  isRequired={true}
+                  variant="auth"
+                  id="pass"
+                  fontSize="sm"
+                  placeholder="Enter your password"
+                  mb="24px"
+                  size="lg"
+                  h="54px"
+                  borderColor={borderColor}
+                  fontWeight="500"
+                  _placeholder={{ placeholderColor }}
+                  type={show ? 'text' : 'password'}
+                  value={password}
+                  onChange={handlePasswordChange}
                 />
-              </InputRightElement>
-            </InputGroup>
-            <Button
-              type="submit"
-              variant="primary"
-              py="20px"
-              px="16px"
-              fontSize="sm"
-              borderRadius="45px"
-              mt={{ base: '20px', md: '0px' }}
-              w="100%"
-              h="54px"
-              mb="24px"
-            >
-              Create your Account
-            </Button>
-          </FormControl>
+                <InputRightElement display="flex" alignItems="center" mt="4px">
+                  <Icon
+                    color={textColorSecondary}
+                    _hover={{ cursor: 'pointer' }}
+                    as={show ? RiEyeCloseLine : MdOutlineRemoveRedEye}
+                    onClick={handleClick}
+                  />
+                </InputRightElement>
+              </InputGroup>
+              {/* CONFIRM */}
+              <FormLabel
+                cursor="pointer"
+                htmlFor="confirm"
+                ms="4px"
+                fontSize="sm"
+                fontWeight="500"
+                borderColor={borderColor}
+                color={textColor}
+                display="flex"
+              >
+                Confirm Password<Text color={brandStars}>*</Text>
+              </FormLabel>
+              <InputGroup size="md">
+                <Input
+                  isRequired={true}
+                  variant="auth"
+                  fontSize="sm"
+                  placeholder="Enter your password again"
+                  id="confirm"
+                  mb="24px"
+                  size="lg"
+                  borderColor={borderColor}
+                  h="54px"
+                  fontWeight="500"
+                  _placeholder={{ placeholderColor }}
+                  type={show ? 'text' : 'password'}
+                  value={confirmPassword}
+                  onChange={handleConfirmPasswordChange}
+                />
+                <InputRightElement display="flex" alignItems="center" mt="4px">
+                  <Icon
+                    color={textColorSecondary}
+                    _hover={{ cursor: 'pointer' }}
+                    as={show ? RiEyeCloseLine : MdOutlineRemoveRedEye}
+                    onClick={handleClick}
+                  />
+                </InputRightElement>
+              </InputGroup>
+              <Button
+                type="submit"
+                variant="primary"
+                py="20px"
+                px="16px"
+                fontSize="sm"
+                borderRadius="45px"
+                mt={{ base: '20px', md: '0px' }}
+                w="100%"
+                h="54px"
+                mb="24px"
+              >
+                Create your Account
+              </Button>
+            </FormControl>
+          </form>
           <Flex justifyContent="center" alignItems="start" maxW="100%" mt="0px">
             <Text color={textColorDetails} fontWeight="500" fontSize="sm">
               Already have an account?
