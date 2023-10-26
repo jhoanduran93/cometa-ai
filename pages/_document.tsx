d8d65a295fcedec42e44247f59ccefded6b844d9
jhoanduran93 <jhoanduran933@gmail.com>
2023-10-26 03:13:20 -0300

    m

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,19 +1,28 @@
 import { Html, Head, Main, NextScript } from 'next/document';
-import Script from 'next/script';
-import Hotjar from '@hotjar/browser';
-
-const siteId = 3710371;
-const hotjarVersion = 6;
-
-Hotjar.init(siteId, hotjarVersion);
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
         <title>Cometa AI ChatBot</title>
-        {/* <meta name="description" content="" /> */}
+        {/* Otros metadatos y enlaces */}
         <link rel="icon" type="image/x-icon" href="/cometa.ico" />
+
+        {/* Agrega el script de Hotjar aquí */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid: 3710371, hjsv: 6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            `
+          }}
+        />
       </Head>
       <body suppressHydrationWarning={true}>
         <Main />
@@ -22,3 +31,4 @@ export default function Document() {
     </Html>
   );
 }
+
