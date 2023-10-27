5fe0a67cb809e809c4ae4eb319855915882a4f1a
jhoanduran93 <jhoanduran933@gmail.com>
2023-10-27 01:28:06 -0300

    modificacion

--- pages/chat/index.tsx
+++ pages/chat/index.tsx
@@ -69,6 +69,20 @@ function App() {
   ];
 
   useEffect(() => {
+
+    const hotjarScript = document.createElement('script');
+      hotjarScript.innerHTML = `
+        (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:3710371,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+      `;
+      document.head.appendChild(hotjarScript);
+
     const ws = new WebSocket('wss://cometa-c40d5067bfcf.herokuapp.com/chatbot');
     setWebsocket(ws);
 
@@ -95,9 +109,9 @@ function App() {
           }
         }
       }
-    };
-    setInputMessage(' ');
-    return () => {
+     };
+     setInputMessage(' ');
+     return () => {
       ws.close();
     };
   }, []);
